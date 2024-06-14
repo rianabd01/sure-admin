4e6d45c144a1901095ee219055bd36fb6edf8125
rianabd01 <rianwahyuab@gmail.com>
2024-06-14 07:31:12 +0700

    fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header({ token, logout }) {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link
-            to="/sure-admin/"
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
@@ -63,7 +63,7 @@ export default function Header({ token, logout }) {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
-                  to="/sure-admin/list"
+                  to="/list"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   aria-current="page"
                 >
@@ -72,7 +72,7 @@ export default function Header({ token, logout }) {
               </li>
               <li>
                 <Link
-                  to="/sure-admin/proof"
+                  to="/proof"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Bukti Pembersihan
