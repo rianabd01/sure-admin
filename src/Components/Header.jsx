d1abedbbecfdee5993a0702032a19ebfe00e749f
rianabd01 <rianwahyuab@gmail.com>
2024-06-14 07:23:06 +0700

    fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 export default function Header({ token, logout }) {
@@ -21,14 +21,14 @@ export default function Header({ token, logout }) {
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="/sure-admin/"
+          <Link
+            to="/sure-admin/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               SURE ADMIN
             </span>
-          </a>
+          </Link>
           <button
             onClick={toggleMenu}
             data-collapse-toggle="navbar-default"
@@ -62,21 +62,21 @@ export default function Header({ token, logout }) {
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="/sure-admin/list"
+                <Link
+                  to="/sure-admin/list"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   aria-current="page"
                 >
                   Laporan Sampah
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/sure-admin/proof"
+                <Link
+                  to="/sure-admin/proof"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Bukti Pembersihan
-                </a>
+                </Link>
               </li>
               <li>
                 {token && (
